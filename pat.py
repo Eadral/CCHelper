18c772c1fe96de3e88114fb9fb941fdca59a98f6
Eadral <a250014238@me.com>
2019-09-30 16:21:33 +0800

    add redirect

--- pat.py
+++ pat.py
@@ -7,6 +7,7 @@ import sys
 testfile_dir = "testfiles"
 outputfile_dir = "outputfiles"
 
+
 def get_dirs():
     dirs = []
     for name in os.listdir("."):
@@ -16,38 +17,41 @@ def get_dirs():
     dirs.remove(outputfile_dir)
     return dirs
 
+
 def get_testfiles():
     return os.listdir(testfile_dir)
+
 
 def get_exe_path(dir_name):
     for name in os.listdir(dir_name):
         if name.endswith(".exe"):
             return name
 
-def get_exe_output(dir_name, testfile):
+
+def get_exe_output(dir_name, testfile, redirect=False):
     shutil.copy(os.path.join(testfile_dir, testfile), os.path.join(dir_name, "testfile.txt"))
     exe_path = get_exe_path(dir_name)
     if os.path.exists(os.path.join(dir_name, "output.txt")):
         os.remove(os.path.join(dir_name, "output.txt"))
     os.chdir(dir_name)
-    os.system(exe_path)
+    os.system("{} > output.txt < testfile.txt".format(exe_path) if redirect else exe_path)
     os.chdir("..")
     rnd = time.time()
     outputfile_name = "output_{}.txt".format(rnd)
     shutil.copy(os.path.join(dir_name, "output.txt"), os.path.join(outputfile_dir, outputfile_name))
     lines = open(os.path.join(outputfile_dir, outputfile_name), encoding="utf-8").readlines()
     lines = list(map(lambda x: x, lines))
-    while lines[-1] == "":
+    while len(lines) > 0 and lines[-1] == "":
         del lines[-1]
     return lines
 
-if __name__ == "__main__":
+def pat(redirect):
     ncases = 0
     npassed = 0
     dirs = get_dirs()
     for testfile in get_testfiles():
         for i in range(0, len(dirs)):
-            for j in range(i+1, len(dirs)):
+            for j in range(i + 1, len(dirs)):
                 ncases += 1
                 lhs = dirs[i]
                 rhs = dirs[j]
@@ -63,3 +67,5 @@ if __name__ == "__main__":
 
     print("passed: {}/{}".format(npassed, ncases))
 
+if __name__ == "__main__":
+    pat(redirect=False)
