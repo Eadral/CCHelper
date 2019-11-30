5393222f0ff04bd9e94dc2f5ad5e2adba03ead3e
Eadral <a250014238@me.com>
2019-11-30 20:43:29 +0800

    add a score system

--- mips_test.py
+++ mips_test.py
@@ -15,19 +15,31 @@ init(autoreset=True)
 def get_test_names(test_set):
     return os.listdir(test_set)
 
+score_sum = 0
+cycles_sum = 0
 
-def run_unit(dir_name, test, test_set):
+def run_unit(dir_name, test, test_set, option=None):
     try:
         testfile_name = os.path.join(test_set, test)
         output_name = os.path.join(test_set, test, "output.txt")
 
         # lhs_out = get_exe_output(dir_name, testfile_name, "error.txt")
         lhs_out, cycles = get_mips_output(dir_name, testfile_name)
+        ref_cycles_path = os.path.join(test_set, test, "ref_cycles")
+
         lhs_out = remove_space(lhs_out)
         rhs_out = remove_space(open(output_name).readlines())
 
         if operator.eq(lhs_out, rhs_out):
-            print(Fore.GREEN + "passed: {}. Cycles: {}".format(test, cycles))
+            if not os.path.exists(ref_cycles_path):
+                os.system("echo {} > {}".format(cycles, ref_cycles_path))
+            ref_cycles = int(open(ref_cycles_path).readline())
+            score = cycles / ref_cycles
+            global cycles_sum, score_sum
+            cycles_sum += cycles
+            score_sum += score * cycles
+
+            print(Fore.GREEN + "passed: {}. Cycles: {}, Score: {:.2f}".format(test, cycles, score))
             return True
         else:
             print(Fore.LIGHTRED_EX + "failed: {}".format(test))
@@ -50,15 +62,17 @@ def run_unit(dir_name, test, test_set):
         return False
 
 
-def unit(dir_name, test_set):
+def unit(dir_name, test_set, option=None):
     ncases = 0
     npassed = 0
     for test in get_test_names(test_set):
         ncases += 1
-        if run_unit(dir_name, test, test_set):
+        if run_unit(dir_name, test, test_set, option):
             npassed += 1
     if ncases == npassed:
-        print(Fore.GREEN + "Passed {}/{}".format(npassed, ncases))
+        global cycles_sum, score_sum
+        weighted_score = score_sum / cycles_sum
+        print(Fore.GREEN + "Passed {}/{}, Weighted Score: {:.2f}".format(npassed, ncases, weighted_score))
     else:
         print(Fore.LIGHTRED_EX + "Passed {}/{}".format(npassed, ncases))
 
@@ -71,5 +85,8 @@ if __name__ == "__main__":
     if len(sys.argv) <= 3:
         unit(sys.argv[1], sys.argv[2])
     else:
-        run_unit(sys.argv[1], sys.argv[3], sys.argv[2])
+        if sys.argv[3][0] == '-':
+            unit(sys.argv[1], sys.argv[2], sys.argv[3][1:])
+        else:
+            run_unit(sys.argv[1], sys.argv[3], sys.argv[2])
         # pass
