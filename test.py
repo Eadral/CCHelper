bff94ff396be913f82a8fb56ad1f79f8a878d0d4
Eadral <a250014238@me.com>
2019-12-23 20:55:41 +0800

    better error test

--- test.py
+++ test.py
@@ -21,24 +21,57 @@ def unit(dir_name, test_set):
         lhs_out = get_exe_output(dir_name, testfile_name, "error.txt")
         rhs_out = remove_space(open(output_name).readlines())
 
-        if operator.eq(lhs_out, rhs_out):
-            print(Fore.GREEN + "passed: {}".format(test))
-            npassed += 1
-        else:
-            print(Fore.LIGHTRED_EX + "failed: {}".format(test))
-            length = min(len(lhs_out), len(rhs_out))
-            for i in range(length):
-                if lhs_out[i] != rhs_out[i]:
-                    print(Fore.LIGHTRED_EX + "found {} while expected '{}' at line {}"
-                          .format("'{}'".format(lhs_out[i]), "'{}'".format(rhs_out[i]), i+1))
-                    exit(-1)
-            if len(lhs_out) > len(rhs_out):
-                print(Fore.LIGHTRED_EX + "found {} while expected {} at line {}"
-                      .format("'{}'".format(lhs_out[length]), "NOTHING", length + 1))
-            else:
-                print(Fore.LIGHTRED_EX + "found {} while expected {} at line {}"
-                      .format("NOTHING", "'{}'".format(rhs_out[length]), length + 1))
-            exit(-1)
+        if lhs_out[0] == rhs_out[0]:
+            is_correct = True
+            for line in rhs_out:
+                # print(line)
+                if line not in lhs_out:
+                    is_correct = False
+            if is_correct:
+                # correct
+                print(Fore.GREEN + "passed: {}".format(test))
+                continue
+        # error
+        print(Fore.LIGHTRED_EX + "failed: {}".format(test))
+        if lhs_out[0] == rhs_out[0]:
+            print(Fore.LIGHTRED_EX + "first line error: expected '{}' while found '{}'".format(rhs_out[0], lhs_out[0]))
+            continue
+        for line in rhs_out:
+            if line not in lhs_out:
+                print(Fore.LIGHTRED_EX + "could not find '{}'".format(line))
+                #               .format("'{}'".format(lhs_out[i]), "'{}'".format(rhs_out[i]), i + 1))
+        # length = min(len(lhs_out), len(rhs_out))
+        # for i in range(length):
+        #     if lhs_out[i] != rhs_out[i]:
+        #         print(Fore.LIGHTRED_EX + "found {} while expected '{}' at line {}"
+        #               .format("'{}'".format(lhs_out[i]), "'{}'".format(rhs_out[i]), i + 1))
+                # exit(-1)
+        # if len(lhs_out) > len(rhs_out):
+        #     print(Fore.LIGHTRED_EX + "found {} while expected {} at line {}"
+        #           .format("'{}'".format(lhs_out[length]), "NOTHING", length + 1))
+        # else:
+        #     print(Fore.LIGHTRED_EX + "found {} while expected {} at line {}"
+        #           .format("NOTHING", "'{}'".format(rhs_out[length]), length + 1))
+        # exit(-1)
+
+        # if operator.eq(lhs_out, rhs_out):
+        #     print(Fore.GREEN + "passed: {}".format(test))
+        #     npassed += 1
+        # else:
+        #     print(Fore.LIGHTRED_EX + "failed: {}".format(test))
+        #     length = min(len(lhs_out), len(rhs_out))
+        #     for i in range(length):
+        #         if lhs_out[i] != rhs_out[i]:
+        #             print(Fore.LIGHTRED_EX + "found {} while expected '{}' at line {}"
+        #                   .format("'{}'".format(lhs_out[i]), "'{}'".format(rhs_out[i]), i+1))
+        #             exit(-1)
+        #     if len(lhs_out) > len(rhs_out):
+        #         print(Fore.LIGHTRED_EX + "found {} while expected {} at line {}"
+        #               .format("'{}'".format(lhs_out[length]), "NOTHING", length + 1))
+        #     else:
+        #         print(Fore.LIGHTRED_EX + "found {} while expected {} at line {}"
+        #               .format("NOTHING", "'{}'".format(rhs_out[length]), length + 1))
+        #     exit(-1)
 
 
 if __name__ == "__main__":
